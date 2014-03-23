8ea9a0a0daeb6771ebc0ed635ec3666f6706dab1
root <root@les>
2014-03-24 01:13:11 +0300

    By kabwogi

--- application/views/user_admission_v.php
+++ application/views/user_admission_v.php
@@ -1,132 +1,43 @@
 <?php include 'include/header.php';?>
-<div id="page-wrapper">
-      <div class="span12">
-        <div class="tabcordion tabs-left tabbable">
-            <ul class="nav nav-tabs">
-              <li class="<?php if(isset($active1)){echo 'active';}?>">
-                <a data-target=".course" data-toggle="tab">Course selection</a>
-              </li>
-              <li class="<?php if(isset($active2)){echo 'active';}?>">
-                <a data-target=".home" data-toggle="tab">Personal information</a>
-              </li>
-              <li class="<?php if(isset($active3)){echo 'active';}?>">
-                <a data-target=".emprecod" data-toggle="tab">Employement Details</a>
-              </li>
-              <li class="<?php if(isset($active4)){echo 'active';}?>">
-                <a data-target=".accadem" data-toggle="tab">Education Background</a>
-              </li>
-              <li class="<?php if(isset($active5)){echo 'active';}?>">
-                <a data-target=".reff" data-toggle="tab">Refferees Details</a>
-              </li>
-              <li class="<?php if(isset($active6)){echo 'active';}?>">
-                <a data-target=".addit" data-toggle="tab">Additional Details</a>
-              </li>
-              <li class="<?php if(isset($active7)){echo 'active';}?>">
-                <a data-target=".settings" data-toggle="tab">Upload Documents</a>
-              </li>
-            </ul>
-        <div class="tab-content" style="display: block;">
-            
-             <div class="course  in tab-pane <?php if(isset($active1)){echo 'active';}?>">
-              <div class='pantop'><h4>Course selection</h4></div>
-              <p >
-                 <div class='tbs'> 
-                    <?php echo form_open('application'); ?>
-                    
-                <div><p><?php echo form_error('college','<div class="error">','</div>'); ?>
-                    <label for="college">College Selection</label>
-                    <select name="college"  class="form-control">
-                        <option > <?php display_input('college',$Ucollege);?></option>
-                      <option >College of Information and Communication Technology</option>
-                      <option >College of Natural and Applied Scince</option>
-                      <option >College of Engeneering</option>
-                    </select></p>
-               </div>
-                <div><p>
-                    <?php echo form_error('course','<div class="error">','</div>'); ?>
-                     <label for="course"> Select Course</label>
-                     <select class="form-control" name="course">
-                        <option >
-                                  <?php display_input('course',$Ucourse);?>
-                        </option>
-                        <option >Master of Science in Computer Science</option>
-                        <option >Master of Science in Health Informatics</option>
-                        <option >Master of Science in Electronics Engineering and Information Technology</option>
-                        <option >Master of Science in Electronics Science and Communication</option>
-                        <option >Master of Science in Telecommunications Engineering</option>
-                     </select>
-                     </p>
-               </div>
-                <div>
-                    <p>
-                       <label> Programme mode (choose one)</label>
-                        <table class="table col-md-3 table-striped">
-                            <tr>
-                                <td> Regular </td>
-                                <td><input  name="chkp" type="radio" value="regular" checked=""
-                                    <?php display_checks('chkp',$prog_mod,'regular');?> 
-                                 ></td>
-                            </tr>
-                            <tr>
-                                <td> Evening</td>
-                                <td><input  name="chkp" type="radio" value="evening"
-                                <?php display_checks('chkp',$prog_mod,'evening');?>
-                                ></td>
-                            </tr>
-                            
-                             <tr>
-                                <td>Other (Specify)</td>
-                                <td><input  name="chkp" type="radio" value="other"
-                                 <?php display_checks('chkp',$prog_mod,'other');?>
-                                ></td>
-                            </tr>
-                             <tr><td colspan=2>
-                             <?php echo form_error('checktext','<div class="error">','</div>'); ?>
-                             <textarea class="form-control" rows="3" name='checktext'>
-                                
-                             </textarea>
-                             </td></tr>
-                             <tr>
-                                <td>
-                                 <input class="subtn btn-primary" type="submit" name="save" value="Save">   
-                                </td>
-                                <td>
-                                  <input class="subtn btn-primary" type="submit" name="savcont" value="Save and continue">
-                                </td>
-                             </tr>
-                        </table>
-                         
-                        
-                    </p>
-                   
-                </div>
-                 </div>
-              
-                    </form>
-              </p>
-            </div>
-            hi
-    </div>
-<?php
-function display_input($filname,$varname){
-         if(set_value($filname)){
-            echo set_value($filname);
-            }elseif(isset($varname)){
-                echo $varname;
-            }
-}
-function display_checks($filname,$varname,$value){
-         if(isset($_POST[$filname])){
-             if($_POST[$filname]==$value){
-                echo 'checked=""';
-                }
-            }elseif(isset($varname)){
-                if($varname==$value){
-                echo 'checked=""';
-                }
-            }
-       }
-?>
 
+<div id="wrapper">
+   
 </div>
-<?php include 'footer.php';?>   
+<div id="page-wrapper">
+    <div>
+        <center>
+            
+            <p>
+                
+            </p>
+        </center>
+    </div>
+       <button type="button" class="btn btn-default btn-lg btn-block">Submitted Thesis</button>
+       <table class="mytable" border="2">
+           
+           <tr>
+               <td colspan="2" align="center" >
+                  1
+               </td>
+                <td colspan="2" align="center" >
+                  2
+               </td>
+                <td colspan="2" align="center" >
+                  3
+               </td>
+           </tr>
+       </table>
+       <form action="http://localhost/pgis/index.php/user_admission_c/do_upload" method="post" accept-charset="utf-8" enctype="multipart/form-data">  
+           <table>
+            <tbody><tr>
+               <td><input type="file" name="userfile" size="20"></td>
+               <td> <span class="glyphicon glyphicon-circle-arrow-up"></span><input type="submit" value="upload"></td>
+            </tr>
+          </tbody>
+           </table>
+ 
+          </form>   
+    </div>
+
+
+    <?php include 'include/footer.php';?>   
