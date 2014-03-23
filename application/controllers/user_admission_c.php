9a3e5edfb9677d4d35c50c66f757d6dbfcbc4c3e
Kabwogi <>
2014-03-23 11:48:03 +0300

    By kabwogi

---
+++ application/controllers/user_admission_c.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_admission_c extends CI_Controller{
+    
+    function load_view(){
+        
+        $this->load->view('user_admission_v');
+    }
+    
+    
+}
---
+++ application/views/user_admission_v.php
@@ -0,0 +1,5 @@
+<?php include 'include/header.php';?>
+
+
+
+<?php include 'include/footer.php';?>	
