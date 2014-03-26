5618bfe5c50376a35f3f2f1bc5b67b8dfd8a8fe0
root <root@les>
2014-03-26 21:40:48 +0300

    By kabwogi

--- application/controllers/user_admission_c.php
+++ application/controllers/user_admission_c.php
@@ -1,6 +1,14 @@
 <?php
 
 class User_admission_c extends CI_Controller{
+    function __construct() {
+        parent::__construct();
+        $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+        $this->load->helper(array('form','html','url','array'));
+        $this->load->library(array('session'));
+        $this->load->helper('directory');
+        $this->load->helper('download');
+    }
     
     function load_view(){
         
@@ -11,8 +19,12 @@ class User_admission_c extends CI_Controller{
         
         $this->load->view('user_admission_v');
     }
-    function do_download(){
+    function do_download($idname){
         
-        $this->load->helper(download);
+        
+        $data = file_get_contents("uploads/emanoble/one.pdf"); // Read the file's contents
+        $name = 'dodoso.pdf';
+        force_download($name, $data);
+
     }
 }
